1633dc467c44e8d9ea721084a3f8c5318ff1c2bb
jimmyjohnjr1203 <68567198+jimmyjohnjr1203@users.noreply.github.com>
2020-09-05 13:38:23 -0700

    Update Zoom.py
    Added support for choosing which day of the week to have a meeting and having it only open one meeting before being needed to be run again.

--- Zoom.py
+++ Zoom.py
@@ -1,54 +1,73 @@
 import keyboard, time, subprocess
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, date
+import pyscreeze
 import pyautogui
 
 #reading the meeting details
 df = pd.read_csv('meetingschedule.csv')
 df_new = pd.DataFrame()
+# check the date
+current_date = date.today()
+day_of_week = current_date.isoweekday()
+print(str(day_of_week))
+# only look at the meetings happening today
+if day_of_week < 4: 
+    todays_meetings = df[df['First_Day'] == day_of_week]
+else:
+    todays_meetings = df[df['Second_Day'] == day_of_week]
 
-while(True):
+check_time = True
+while(check_time == True):
     #Check the current system time
-    timestr = datetime.now().strftime("%H:%M")
-    #Check if the current time is mentioned in the Dataframe
-    if timestr in df.Time.values:
-        df_new = df[df['Time'].astype(str).str.contains(timestr)]
-        #Open the Zoom app
-        subprocess.Popen("C:\\Users\\ZEESHAN\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
-        time.sleep(15)
-        #Locate the position of the join button on the screen
-        position = pyautogui.locateOnScreen("buttons\\join_button.png")
-        #Move the cursor to the position of the button
-        pyautogui.moveTo(position)
-        #Perform click operation
-        pyautogui.click()
-        time.sleep(2)
-        #Write the meeting ID from the dataframe onto the Zoom App
-        keyboard.write(df_new.iloc[0,1])
+    timestr = datetime.now().strftime("%H:%M") 
+    #Check if the current time is mentioned in the Dataframe and end loop
+    if timestr in todays_meetings.Time.values:
+        print('Time found in database')
+        check_time = False
 
-        #For tapping the Turn off video option on Zoom app
-        position = pyautogui.locateOnScreen("buttons\\turn_off_vid_button.png")
-        pyautogui.moveTo(position)
-        pyautogui.click()
-        time.sleep(2)
+print('Opening Zoom')
+df_new = todays_meetings[todays_meetings['Time'].astype(str).str.contains(timestr)]
+print (df_new)
+#Open the Zoom app
+subprocess.Popen("C:\\Users\\jackl\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
+print('Zoom opened')
+time.sleep(3)
+#Locate the position of the join button on the screen
+position = pyautogui.locateCenterOnScreen("buttons\\join_button.png")
+#Move the cursor to the position of the button
+pyautogui.moveTo(position[0], position[1])
+#Perform click operation
+pyautogui.click()
+print('Join selected')
+time.sleep(3)
+#Write the meeting ID from the dataframe onto the Zoom App
+keyboard.write(df_new.iloc[0,1])
+print('MeetingId placed')
 
-        #For tapping on the Join button
-        position = pyautogui.locateOnScreen("buttons\\join_button_2.png")
-        pyautogui.moveTo(position)
-        pyautogui.click()
-        time.sleep(7)
+#For tapping the Turn off video option on Zoom app
+position = pyautogui.locateCenterOnScreen("buttons\\turn_off_vid_button.png")
+pyautogui.moveTo(position[0], position[1])
+pyautogui.click()
+time.sleep(2)
 
-        #Reads the Meeting Passcode from the dataframe and enters into the zoom app
-        keyboard.write(str(int(df_new.iloc[0,2])))
-        time.sleep(3)
+#For tapping on the Join button
+position = pyautogui.locateCenterOnScreen("buttons\\join_button_2.png")
+pyautogui.moveTo(position[0], position[1])
+pyautogui.click()
+time.sleep(7)
 
-        #For finally joining the meeting
-        position = pyautogui.locateOnScreen("buttons\\join_meeting.png")
-        pyautogui.moveTo(position)
-        pyautogui.click()
+#Reads the Meeting Passcode from the dataframe and enters into the zoom app
+keyboard.write(str(df_new.iloc[0,2]))
+time.sleep(3)
 
-        #Wait for one minute before the next iteration starts
-        time.sleep(60)
+#For finally joining the meeting
+position = pyautogui.locateOnScreen("buttons\\join_meeting.png")
+pyautogui.moveTo(position[0], position[1])
+pyautogui.click()
+
+#Wait for one minute before the next iteration starts
+#time.sleep(60)
 
 
 
